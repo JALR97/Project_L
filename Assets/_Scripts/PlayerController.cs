3e8fe6045fc45fa7dd1a1b247a7dfaeadabf6e4e
Nonestdeus <edersaiv@gmail.com>
2023-07-05 03:13:56 -0500

    Se mueve bien Excepto cuando presiona S
    Todo va bien hasta que intento caminar hacía atras

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -34,15 +34,22 @@ public class PlayerController : MonoBehaviour
         moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
         if( moveDirection.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.y) * Mathf.Rad2Deg + camera.eulerAngles.y;
+            
+            float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.y) * Mathf.Rad2Deg + camera.eulerAngles.y ;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            Vector3 move = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-
+            
             
 
-            rb.AddForce(move * speedPlayer * Time.deltaTime, ForceMode.Force);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+
+
+            Vector3 move = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            float a = 1f;
+            if (Input.GetKeyDown(KeyCode.S))
+                a = -a;
+
+            rb.velocity = move.normalized * speedPlayer* Time.deltaTime;
         }
 
     }
