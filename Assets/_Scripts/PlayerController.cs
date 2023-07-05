6f9880c8175e2995a93569cda264a0b4da5fd3a0
Nonestdeus <edersaiv@gmail.com>
2023-07-05 02:22:25 -0500

    movimiento bugeado

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float speedPlayer = 10f;
+    public float speedPlayer = 10f;
     float horizontalInput;
     float verticalInput;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity = 1.0f;
     Rigidbody rb;
     Vector3 moveDirection;
 
-    public Transform cameraDirection;
+    public Transform camera;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -22,14 +24,24 @@ public class PlayerController : MonoBehaviour
         verticalInput = Input.GetAxisRaw("Vertical");
 
     }
-
+    
     private void FixedUpdate()
     {
         MovePlayer();
     }
     void MovePlayer()
     {
-        moveDirection = cameraDirection.forward * verticalInput + cameraDirection.right * horizontalInput;
-        rb.AddForce(moveDirection.normalized * speedPlayer * 10f, ForceMode.Force);
+        moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+        if( moveDirection.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.y) * Mathf.Rad2Deg + camera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 move = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            rb.AddForce(move.normalized  * speedPlayer * Time.deltaTime, ForceMode.Force);
+        }
+
     }
 }
