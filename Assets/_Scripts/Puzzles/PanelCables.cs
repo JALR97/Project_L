8e4b9b5ca5305e0cd81d136d757c1f0caa6444a6
Jose Angel Lugo <Joseangel@HopefulUndertone.games>
2023-07-05 17:05:04 -0500

    Idea inicial del funcionamiento y estructura del puzzle de cables

---
+++ Assets/_Scripts/Puzzles/PanelCables.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelCables : MonoBehaviour
+{
+    //**    ---Components---    **//
+    //  [[ set in editor ]] 
+    
+    //  [[ set in Start() ]] 
+    
+    
+    //**    ---Variables---    **//
+    //  [[ balance control ]] 
+    
+    //  [[ internal work ]] 
+    
+    
+    //**    ---Properties---    **//
+    
+    
+    //**    ---Functions---    **//
+}
---
+++ Assets/_Scripts/Puzzles/PuzzleCables.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleCables : MonoBehaviour
+{
+    //**    ---Components---    **//
+    //  [[ set in editor ]] 
+    
+    //  [[ set in Start() ]] 
+    
+    
+    //**    ---Variables---    **//
+    //  [[ balance control ]] 
+    
+    //  [[ internal work ]] 
+    
+    
+    //**    ---Properties---    **//
+    
+    
+    //**    ---Functions---    **//
+}
