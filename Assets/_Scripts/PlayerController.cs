2689661d2ff186f30ffae3b9a251f1863a775c0f
Jose Angel Lugo <Joseangel@HopefulUndertone.games>
2023-07-06 20:57:08 -0500

    Player Movement and Camera fixes
    Integracion entre el player manager y el player controller, mas algunos cambios en el comportamiento de la camara y estructura del script playercontroller

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -4,51 +4,45 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speedPlayer = 10f;
-    float horizontalInput;
-    float verticalInput;
-    public float turnSmoothTime = 0.1f;
-    Rigidbody rb;
-    Vector3 moveDirection;
-
-    public Transform thirdPersonCamera;
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
-    }
-    void Update()
+    //public float turnSmoothTime = 0.1f; **Never used?
+    
+    //Componentes
+    [SerializeField] private PlayerManager playerManager;
+    [SerializeField] private Transform thirdPersonCamera;
+    [SerializeField] private Rigidbody rb;
+    
+    //variables de balance
+    [SerializeField] private float speed = 20f;
+    
+    //variables de trabajo
+    private Vector3 moveDirection;
+    private float horizontalInput;
+    private float verticalInput;
+    
+    //Funciones
+    private void FixedUpdate()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
-
-    }
-    
-    private void FixedUpdate()
-    {
-        MovePlayer();
-    }
-    void MovePlayer()
-    {
         moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
-        if( moveDirection.magnitude >= 0.1f)
-        {
-            
-            float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.y) * Mathf.Rad2Deg + thirdPersonCamera.eulerAngles.y ;            
-            
-
-            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
-
-            Vector3 move = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-
-            Vector3 cameraForward = thirdPersonCamera.transform.forward;
-            cameraForward.y = 0f;  // Mantener el movimiento en el plano horizontal
-            cameraForward.Normalize();
-
-            Vector3 movement = (horizontalInput * thirdPersonCamera.transform.right + verticalInput * cameraForward).normalized;
-            movement.y = 0f;
-            rb.velocity = movement * speedPlayer* Time.deltaTime;
+        
+        if (moveDirection.magnitude > 0 && playerManager.CanWalk()) {
+            MovePlayer();
         }
+    }
+    void MovePlayer() {
+        float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.y) * Mathf.Rad2Deg + thirdPersonCamera.eulerAngles.y ;            
+            
+        transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
 
+        Vector3 move = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+        Vector3 cameraForward = thirdPersonCamera.transform.forward;
+        cameraForward.y = 0f;  // Mantener el movimiento en el plano horizontal
+        cameraForward.Normalize();
+
+        Vector3 movement = (horizontalInput * thirdPersonCamera.transform.right + verticalInput * cameraForward).normalized;
+        movement.y = 0f;
+        rb.velocity = movement * (speed * Time.deltaTime);
     }
 }
